b6db403378dfc2469fc8fb4758ae6fc22e882210
kelvindong1608 <hoangan_kos@yahoo.com>
2017-08-24 10:37:20 +1200

    using para and fucntion

---
+++ Diskinfo2.ps1
@@ -0,0 +1,20 @@
+﻿<#
+.Synopis
+this is the short explantion 
+.Description
+This is the long description
+.Paramenter COmputerName
+this is for remote computers
+.Example 
+diskinfo -computername remote 
+this is for remore computers
+#>
+function Get-diskinfo {
+[CmdletBinding()]
+param (
+    [parameter(Mandatory=$True)]
+    [string[]]$ComputerName,
+    $an
+)
+Get-WmiObject -computername $ComputerName -class win32_logicaldisk -Filter "DeviceID= 'C:'" 
+}
